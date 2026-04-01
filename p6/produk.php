50923d0bfd718a5d53e7538f15d5ffeb3da097e9
akmsy <akmsyyy1@gmail.com>
2026-04-01 13:40:16 +0700

    fetch_object

--- p6/produk.php
+++ p6/produk.php
@@ -11,7 +11,14 @@
                   //alamat server, username, password, nama database
         $sql = "SELECT * FROM products"; // mengambil data dari tabel products
         $produk = $koneksi->query($sql); // mengeksekusi sql menggunakan fungsi query yang melekat pada sebuah koneksi
-        var_dump($produk); // menampilkan hasil query
+        // var_dump($produk->fetch_object()); // menampilkan hasil query
+        // var_dump($produk->fetch_object()); // menampilkan hasil query
+                        // satu baris terambil, lalu diubah menjadi objek
+
+        while($data = $produk->fetch_object()){ // akan melakukan looping. setiap kali looping fetch objek akan dilakukan 1x, akan looping terus sampai produknya habis
+            var_dump($data);
+            print "<br>----------------------<br>";
+        }
     ?>
 </body>
 </html>
