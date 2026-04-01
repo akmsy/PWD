f7eede541ad5e71d02b200f42446e7d2b69aa891
akmsy <akmsyyy1@gmail.com>
2026-04-01 14:01:34 +0700

    menampilkan data berbentuk tabel

--- p6/produk.php
+++ p6/produk.php
@@ -6,28 +6,47 @@
     <title>Produk</title>
 </head>
 <body>
-    <?php 
+    <table>
+
+        <?php 
+        // FUNGSI 1 : membuat koneksi ke database
         $koneksi = new mysqli("localhost", "root", "", "nwind");
-                  //alamat server, username, password, nama database
+        //alamat server, username, password, nama database
         $sql = "SELECT * FROM products"; // mengambil data dari tabel products
+        
+        // FUNGSI 2 : mengeksekusi sql
         $produk = $koneksi->query($sql); // mengeksekusi sql menggunakan fungsi query yang melekat pada sebuah koneksi
         // var_dump($produk->fetch_object()); // menampilkan hasil query
         // var_dump($produk->fetch_object()); // menampilkan hasil query
-                        // satu baris terambil, lalu diubah menjadi objek
-
+        // satu baris terambil, lalu diubah menjadi objek
+        
+        // FUNGSI 3 : menampilkan data produk
         while($data = $produk->fetch_object()){ // akan melakukan looping. setiap kali looping fetch objek akan dilakukan 1x, akan looping terus sampai produknya habis
             // var_dump($data); //1 data satu objek, lalu ditampilkan
             // var_dump($data->ProductName); // menampilkan nama produk saja, karena data sudah dalam bentuk objek, maka bisa langsung memanggil propertinya, tapi string masih kelihatan karena pake var_dump
-            
-            print($data->ProductID); 
-            print(" - ");
-            print($data->ProductName);
-            print(" - ");
-            print($data->UnitPrice); 
-            print "<br>----------------------<br>";
+        ?>
+
+        <tr>
+            <th>PID</th>
+            <th>Nama</th>
+            <th>Harga</th>
+        </tr>
+        
+        <tr>
+            <td><?= $data->ProductID; ?></td>
+            <td><?= $data->ProductName; ?></td>
+            <td><?= $data->UnitPrice; ?></td>
+        </tr>
+
+        <?php 
+            // print($data->ProductID); 
+            // print(" - ");
+            // print($data->ProductName);
+            // print(" - ");
+            // print($data->UnitPrice); 
+            // print "<br>----------------------<br>";
         }
-
-
-    ?>
+        ?>
+    </table>
 </body>
 </html>
